6a2f766cae6610753c320713ddae03769230af0f
Mark Kaylor <mark.kaylor@strapi.io>
2021-07-12 16:02:12 +0200

    create template for next example

---
+++ README.md
@@ -0,0 +1,6 @@
+# Template for Next.js Strapi Example
+
+See here for more information: https://github.com/vercel/next.js/tree/canary/examples/cms-strapi
+
+
+
---
+++ template/config/functions/bootstrap.js
@@ -0,0 +1,157 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime-types");
+const { posts, authors } = require("../../data/data.json");
+
+/**
+ * An asynchronous bootstrap function that runs before
+ * your application gets started.
+ *
+ * This gives you an opportunity to set up your data model,
+ * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
+ */
+
+async function isFirstRun() {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup",
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+}
+
+async function setPublicPermissions(newPermissions) {
+  // Find the ID of the public role
+  const publicRole = await strapi
+    .query("role", "users-permissions")
+    .findOne({ type: "public" });
+
+  // List all available permissions
+  const publicPermissions = await strapi
+    .query("permission", "users-permissions")
+    .find({
+      type: ["users-permissions", "application"],
+      role: publicRole.id,
+    });
+
+  // Update permission to match new config
+  const controllersToUpdate = Object.keys(newPermissions);
+  const updatePromises = publicPermissions
+    .filter((permission) => {
+      // Only update permissions included in newConfig
+      if (!controllersToUpdate.includes(permission.controller)) {
+        return false;
+      }
+      if (!newPermissions[permission.controller].includes(permission.action)) {
+        return false;
+      }
+      return true;
+    })
+    .map((permission) => {
+      // Enable the selected permissions
+      return strapi
+        .query("permission", "users-permissions")
+        .update({ id: permission.id }, { enabled: true });
+    });
+
+  await Promise.all(updatePromises);
+}
+
+function getFileSizeInBytes(filePath) {
+  const stats = fs.statSync(filePath);
+  const fileSizeInBytes = stats["size"];
+  return fileSizeInBytes;
+}
+
+function getFileData(fileName) {
+  const filePath = `./data/uploads/${fileName}`;
+
+  // Parse the file metadata
+  const size = getFileSizeInBytes(filePath);
+  const ext = fileName.split(".").pop();
+  const mimeType = mime.lookup(ext);
+
+  return {
+    path: filePath,
+    name: fileName,
+    size,
+    type: mimeType,
+  };
+}
+
+// Create an entry and attach files if there are any
+async function createEntry({ model, entry, files }) {
+  try {
+    const createdEntry = await strapi.query(model).create(entry);
+    if (files) {
+      await strapi.entityService.uploadFiles(createdEntry, files, {
+        model,
+      });
+    }
+  } catch (e) {
+    console.log("model", entry, e);
+  }
+}
+
+async function importAuthors() {
+  return Promise.all(
+    authors.map(async (author) => {
+      const slugifiedName = author.name.split(" ").join('').toLowerCase()
+      const files = {
+        picture: getFileData(
+          `${slugifiedName}.jpg`
+        ),
+      };
+
+      return createEntry({
+        model: "author",
+        entry: author,
+        files,
+      });
+    })
+  );
+}
+
+async function importPosts() {
+  return Promise.all(
+    posts.map((post) => {
+      const files = {
+        image: getFileData(`${post.slug}.jpg`),
+      };
+      return createEntry({ model: "post", entry: post, files });
+    })
+  );
+}
+
+async function importSeedData() {
+  // Allow read of application content types
+  await setPublicPermissions({
+    post: ["find", "findone"],
+    author: ["find", "findone"],
+  });
+
+  // Create all entries
+  await importAuthors();
+  await importPosts();
+}
+
+module.exports = async () => {
+  const shouldImportSeedData = await isFirstRun();
+
+  if (shouldImportSeedData) {
+    try {
+      console.log("Setting up the template...");
+      await importSeedData();
+      console.log("Ready to go");
+    } catch (error) {
+      console.log("Could not import seed data");
+      console.error(error);
+    }
+  }
+};
